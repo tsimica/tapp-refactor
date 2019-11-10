b3fd0d29786b5054fe66d3071871c3c05751a1af
Stephen Tsimicalis <tsimica@users.noreply.github.com>
2019-11-10 16:55:11 -0500

    Prepare spec files for testing

--- spec/controllers/position_templates_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe PositionTemplatesController, type: :controller do
-
-end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::Application, type: :model do
+RSpec.describe Application, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
