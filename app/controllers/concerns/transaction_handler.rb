649809b8a1bc2007d32641ce4e49e3297e054c80
Stephen Tsimicalis <tsimica@users.noreply.github.com>
2019-11-11 02:17:13 -0500

    Add transaction handler

---
+++ app/controllers/concerns/transaction_handler.rb
@@ -0,0 +1,12 @@
+module TransactionHandler
+  extend ActiveSupport::Concern
+
+  def start_transaction(&block)
+    ActiveRecord::Base.transaction do
+      block.call
+    rescue StandardError => e
+        render_error(message: e.errors)
+        raise ActiveRecord::Rollback
+    end
+  end
+end
