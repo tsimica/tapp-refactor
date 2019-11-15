b1d8afe19f72489039aeae6a8a6b7f3525443f0d
Stephen Tsimicalis <tsimica@users.noreply.github.com>
2019-11-15 03:49:32 -0500

    Update spec factory and model documentation for Offer

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -34,6 +34,7 @@ end
 #  url_token               :string
 #  created_at              :datetime         not null
 #  updated_at              :datetime         not null
+#  status                  :integer          default("0"), not null
 #
 # Indexes
 #
