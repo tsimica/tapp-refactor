a2670fff53b06c9136794911ca28e3318967c17f
Stephen Tsimicalis <tsimica@users.noreply.github.com>
2019-11-15 03:50:02 -0500

    Implement all routes for current API spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,43 @@
 Rails.application.routes.draw do
     namespace :api do
         namespace :v1 do
+            # Applicants
+            resources :applicants, only: [:index, :show, :create]
+
+            # Application
+            resources :applications, only: [:index, :create]
+
+            # Assignments
+            resources :assignments, only: [:show, :create] do
+                resources :wage_chunks, controller: 'assignment_wage_chunks', only: [:index, :create]
+                resources :offers, controller: 'active_offers', only: [:show, :create] do
+                    collection do
+                        post :accept
+                        post :reject
+                        post :withdraw
+                        post :email
+                        post :nag
+                    end
+                end
+            end
+
+            # Contract Templates
+            get '/available_contract_templates' => 'contract_templates#available'
+
+            # Instructors
+            resources :instructors, only: [:index, :create] do
+                collection do 
+                    post :delete
+                end
+            end
+
+            # Positions
+            resources :positions, only: [:index, :create] do 
+                collection do
+                    post :delete
+                end
+            end
+
             # Sessions
             resources :sessions, only: [:index, :create] do
                 collection do
@@ -8,20 +45,18 @@ Rails.application.routes.draw do
                 end
                 resources :applicants, only: [:index]
                 resources :applications, only: [:index, :create]
+                resources :assignments, only: [:index]
                 resources :contract_templates, only: [:index, :create]
+                resources :positions, controller: 'session_positions', only: [:index, :create]
             end
 
-            # Contract Templates
-            get '/available_contract_templates' => 'contract_templates#available'
 
-            # Application
-            resources :applications, only: [:index, :create]
-
-            # Applicants
-            resources :applicants, only: [:index, :show]
-
-            # Assignments
-            resources :assignments, only: [:show, :create]
+            # Wage Chunks
+            resources :wage_chunks, only: [:create] do
+                collection do
+                    post :delete
+                end
+            end
         end
     end
 end
