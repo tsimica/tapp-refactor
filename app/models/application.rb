4ad6b51c7eadb292de6df12413cd3a78af118538
Stephen Tsimicalis <tsimica@users.noreply.github.com>
2019-11-18 15:22:36 -0500

    Update association for Application -- belongs_to an Applicant

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,8 +4,8 @@
 class Application < ApplicationRecord
     has_many :position_preferences
     has_many :positions, through: :position_preferences
-    has_one :applicant_data_for_matching, dependent: :destroy
-    has_one :applicant, through: :applicant_data_for_matching
+    belongs_to :applicant
+
     belongs_to :session
 
     scope :all_applications, -> { includes(:applicant_data_for_matching).all.order(:id) }
@@ -16,13 +16,15 @@ end
 #
 # Table name: applications
 #
-#  id         :integer          not null, primary key
-#  session_id :integer          not null
-#  comments   :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :integer          not null, primary key
+#  session_id   :integer          not null
+#  applicant_id :integer          not null
+#  comments     :text
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 # Indexes
 #
-#  index_applications_on_session_id  (session_id)
+#  index_applications_on_applicant_id  (applicant_id)
+#  index_applications_on_session_id    (session_id)
 #
